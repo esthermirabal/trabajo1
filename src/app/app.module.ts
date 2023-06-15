1d008630eaf2f9c4ae29be9fa1c50f25e9ddc375
Esther Mirabal <esther.mirabal.1402@gmail.com>
2023-06-15 20:20:56 -0300

    creaciondecomponentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componente/navbar/navbar.component';
+import { DashboardComponent } from './componente/dashboard/dashboard.component';
+import { FooterComponent } from './componente/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
