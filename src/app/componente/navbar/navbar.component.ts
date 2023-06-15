47a877a937c999a9f46f04eacb76ef88969d13e4
Esther Mirabal <esther.mirabal.1402@gmail.com>
2023-06-15 20:32:46 -0300

    imagen

--- src/app/componente/navbar/navbar.component.ts
+++ src/app/componente/navbar/navbar.component.ts
@@ -7,8 +7,10 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   titulo: string;
+  imagen: string;
   constructor(){
     this.titulo = "INCUYO";
+    this.imagen = "https://i.pinimg.com/474x/ac/64/48/ac6448aa1da2c42e4d13f9c73d909ba2.jpg";
   }
 }
 
